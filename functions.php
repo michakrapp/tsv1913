b8e1d9e0a73ee359c484b2bfe46cb9ed5d39105f
Micha Krapp <michakrapp@gmail.com>
2025-01-12 21:22:59 +0100

    add own default avatar

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 
-// Additional functions for the theme
+/**
+ * Additional functionality for the theme.
+ * 
+ * Before adding check if it should be a plugin.
+ */ 
 
 // disallow file edit in admin
 add_action( 'init', function() {
@@ -9,30 +13,23 @@ add_action( 'init', function() {
     }
 });
 
-
-// Enqueues editor-style.css in the editors.
-if ( ! function_exists( 'tsv1913_editor_style' ) ) :
+if ( ! function_exists( 'tsv1913_editor_style' ) ) {
 	/**
 	 * Enqueues editor-style.css in the editors.
 	 *
 	 * @since tsv1913 1.0
-	 *
-	 * @return void
 	 */
 	function tsv1913_editor_style() {
 		add_editor_style( get_parent_theme_file_uri( 'assets/css/editor-style.css' ) );
 	}
-endif;
+}
 //add_action( 'after_setup_theme', 'tsv1913_editor_style' );
 
-// Enqueues style.css on the front.
-if ( ! function_exists( 'tsv1913_enqueue_styles' ) ) :
+if ( ! function_exists( 'tsv1913_enqueue_styles' ) ) {
 	/**
 	 * Enqueues style.css on the front.
 	 *
 	 * @since tsv1913 1.0
-	 *
-	 * @return void
 	 */
 	function tsv1913_enqueue_styles() {
 		wp_enqueue_style(
@@ -42,5 +39,39 @@ if ( ! function_exists( 'tsv1913_enqueue_styles' ) ) :
 			wp_get_theme()->get( 'Version' )
 		);
 	}
-endif;
+}
 add_action( 'wp_enqueue_scripts', 'tsv1913_enqueue_styles' );
+
+if ( ! function_exists( 'tsv1913_add_default_avatar_option' ) ) {
+	/**
+	 * Add own profile image avatar option.
+	 *
+	 * @since tsv1913 1.0
+	 */
+	function tsv1913_add_default_avatar_option( $avatars ) {
+		$url = get_parent_theme_file_uri() . '/assets/images/avatar-default.png';
+		$avatars[ $url ] = 'TSV Avatar';
+
+		return $avatars;
+	}
+}
+add_filter( 'avatar_defaults', 'tsv1913_add_default_avatar_option' );
+
+if ( ! function_exists( 'tsv1913_fix_default_avatar_url' ) ) {
+	/**
+	 * Fix theme default avatar url since it gets some gravatar prefix.
+	 *
+	 * @since tsv1913 1.0
+	 *
+	 * @return void
+	 */
+	function tsv1913_fix_default_avatar_url( $args ) {
+
+		if ( $args['found_avatar'] === false ) {
+			$args['url'] = $args['default'];
+		}
+
+		return $args;
+	}
+}
+add_filter( 'get_avatar_data', 'tsv1913_fix_default_avatar_url'); 
