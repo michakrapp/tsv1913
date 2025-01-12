b2e392b5fb1919c030f03895c4a16ac0663d781d
Micha Krapp <michakrapp@gmail.com>
2025-01-13 00:41:20 +0100

    new footer copyright pattern

---
+++ patterns/copyright.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Copyright Year
+ * Slug: tsv1913/copyright
+ * Categories: footer
+ */
+?>
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">© <?php echo date_i18n ('Y'); ?> TSV 1913 Harreshausen e.V.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">Designed von <a href="https://mkwp.de" rel="nofollow">MKWP</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"right"} -->
+<p class="has-text-align-right">mit <a href="https://wordpress.org" rel="nofollow">WordPress</a></p>
+<!-- /wp:paragraph -->
